46a31c59643558468bcb37aeeb5517dc6fc53188
gilbrene <1giulsson@gmail.com>
2024-08-23 22:07:13 -0300

    horario feito

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,7 +19,7 @@
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Poppins:wght@400;500;700&display=swap"
         rel="stylesheet" />
 
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
@@ -120,19 +120,34 @@
 
 
         <div class="blackgcinza">
-            <section id="secao-categoria" class="container">
-                <h1><i class="bi bi-clock"></i>Horário de Funcionamento</h1>
-                <img class="logohorario" src="../img/fundohorario.jpg">
-                {{-- <style>
-                    #secao-categoria img {
+            <section id="secao-categoria2" class="container">
+                <h1 class="text-center"><i class="bi bi-clock"></i>Horário de Funcionamento<i class="bi bi-clock"></i></h1>
+                <h2 class="text-center">Seg a Sex
+                    <br>
+                    5h - 0h
+                    <br>
+                    Sáb
+                    <br>
+                    8h - 16h
+                    <br>
+                    Dom/Feriados
+                    <br>
+                    8h - 14h
+                </h2>
+                {{-- <img class="logohorario" src="../img/fundohorario.jpg"> --}}
+                <style>
+                    #secao-categoria2 {
 
-                        border-radius: 2px;
+                        width: 1100px;
+                        height: 300px;
+                        background-image: url('../img/fundohorario.jpg');
+                        border-radius: 30px;
                         background-repeat: no-repeat;
                         /* Impede a repetição da imagem */
                         background-position: center;
                         /* Centraliza a imagem */
                     }
-                </style> --}}
+                </style>
             </section>
         </div>
 
